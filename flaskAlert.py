5d42e12978b442e127bcf8e2fb4864cedaf4d331
Carlos Augusto Malucelli <malucellicarlos@gmail.com>
2018-01-31 10:44:54 -0200

    fix

--- flaskAlert.py
+++ flaskAlert.py
@@ -1,31 +1,40 @@
-#
-# Alertmanager webhook for Telegram using Flask
-#
-import telegram
 from flask import Flask
 from flask import request
+import telegram
+import logging
 
 app = Flask(__name__)
+app.secret_key = 'aYT>.L$kk2h>!'
 
 @app.route('/alert', methods = ['POST'])
 def postAlertmanager():
 
     content = request.get_json()
 
-    message = """
-Status """+content['status']+"""
-    
-Alertname: """+content['commonLabels']['alertname']+"""
+    try:
 
-Instance: """+content['commonLabels']['instance']+"""("""+content['commonLabels']['name']+""")
+        for alert in content['alerts']:
+
+            message = """
+Status """+alert['status']+"""
     
-"""+content['commonAnnotations']['description']+"""
+Alertname: """+alert['labels']['alertname']+"""
+
+Instance: """+alert['labels']['instance']+"""("""+alert['labels']['name']+""")
+    
+"""+alert['annotations']['description']+"""
 """
+            bot = telegram.Bot(token=botToken)
+            bot.sendMessage(chat_id="-chatID",text=message)
+    except:
 
-    bot = telegram.Bot(token='botToken')
-    bot.sendMessage(chat_id="-chatID",text=message)
-        
-    return message
- 
-app.run(host='0.0.0.0', port= 9119)
+        logger = logging.getLogger(__name__)
+        logger.info(content)
+        bot = telegram.Bot(token=botToken)
+        bot.sendMessage(chat_id="-chatID",text="Falhou o envio via Flask para o Telegram")
 
+    return ""
+
+if __name__ == '__main__':
+    logging.basicConfig(filename='flaskAlert.log',level=logging.INFO)
+    app.run(host='0.0.0.0', port= 8090)
