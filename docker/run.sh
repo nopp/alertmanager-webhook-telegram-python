580837125b6f263a00f10d65e83fbf082ab2d182
Carlos Augusto Malucelli <malucellicarlos@gmail.com>
2019-03-12 09:24:00 -0300

    Update run.sh
    fix

--- docker/run.sh
+++ docker/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sed -i s/botToken/"$bottoken"/ flaskAlert.py
-sed -i s/chatID/"$chatid"/ flaskAlert.py
+sed -i s/xchatIDx/"$chatid"/ flaskAlert.py
 
 /usr/bin/gunicorn -w 4 -b 0.0.0.0:9119 flaskAlert:app
