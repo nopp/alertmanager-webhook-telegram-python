30ed8ab481c93d2f3e707c6a2b0699c82d70cfd8
Anatoly Pugachev <matorola@gmail.com>
2019-12-23 13:15:55 +0300

    Be more verbose (in logs) on docker container run. Fail to run if bot token or chatid is not set.

--- docker/run.sh
+++ docker/run.sh
@@ -1,4 +1,31 @@
 #!/bin/bash
+
+if [ "x" == "x$username" ]; then
+  echo "warning: username is not set"
+else
+  echo "admin user is $username"
+fi
+
+if [ "x" == "x$password" ]; then
+  echo "warning: password is not set"
+else
+  echo "password is set (not visible)"
+fi
+
+if [ "x" == "x$bottoken" ]; then
+  echo "FAIL: bottoken is not set"
+  exit 1
+else
+  echo "bottoken is set (not visible)"
+fi
+
+if [ "x" == "x$chatid" ]; then
+  echo "FAIL: chatid is not set"
+  exit 2
+else
+  echo "chatid is $chatid"
+fi
+
 sed -i s/botToken/"$bottoken"/ flaskAlert.py
 sed -i s/xchatIDx/"$chatid"/ flaskAlert.py
 sed -i s/XXXUSERNAME/"$username"/ flaskAlert.py
