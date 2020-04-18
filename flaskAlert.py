2b437d2c0a9aa161ac5446ee582133eab6409ae8
Carlos Augusto Malucelli <camalucelli@gmail.com>
2020-04-18 02:50:08 -0300

    Update flaskAlert.py
    Fixed a lot of things!

--- flaskAlert.py
+++ flaskAlert.py
@@ -1,51 +1,50 @@
-import telegram
-import logging
-import json
+import telegram, json
+from dateutil import parser
 from flask import Flask
 from flask import request
 from flask_basicauth import BasicAuth
 
 app = Flask(__name__)
 app.secret_key = 'aYT>.L$kk2h>!'
+basic_auth = BasicAuth(app)
+
+chatID = "-xchatIDx"
+app.config['BASIC_AUTH_FORCE'] = True
 app.config['BASIC_AUTH_USERNAME'] = 'XXXUSERNAME'
 app.config['BASIC_AUTH_PASSWORD'] = 'XXXPASSWORD'
 
-basic_auth = BasicAuth(app)
-app.config['BASIC_AUTH_FORCE'] = True
 bot = telegram.Bot(token="botToken")
-chatID = "xchatIDx"
 
 @app.route('/alert', methods = ['POST'])
 def postAlertmanager():
-
-    content = json.loads(request.get_data())
-    with open("Output.txt", "w") as text_file:
-        text_file.write("{0}".format(content))
+    
     try:
+        content = json.loads(request.get_data())
         for alert in content['alerts']:
-            message = """Status: """+alert['status']+""" \n"""
-            message += """Alertname: """+alert['labels']['alertname']+""" \n"""
-
-            if alert['status'] == "firing":
-                message += """Detected: """+alert['startsAt']+""" \n"""
-
-            if alert['status'] == "resolved":
-                message += """Resolved: """+alert['endsAt']+""" \n"""
-
+            message = "Status: "+alert['status']+"\n"
             if 'name' in alert['labels']:
-                message += """Instance: """+alert['labels']['instance']+"""("""+alert['labels']['name']+""") \n"""
+                message += "Instance: "+alert['labels']['instance']+"("+alert['labels']['name']+")\n"
             else:
-                message += """Instance: """+alert['labels']['instance']+""" \n"""
-
-            message += """\n\n"""+alert['annotations']['description']+""""""
+                message += "Instance: "+alert['labels']['instance']+"\n"
+            if 'info' in alert['annotations']:
+                message += "Info: "+alert['annotations']['info']+"\n"
+            if 'summary' in alert['annotations']:
+                message += "Summary: "+alert['annotations']['summary']+"\n"                
+            if 'description' in alert['annotations']:
+                message += "Description: "+alert['annotations']['description']+"\n"
+            if alert['status'] == "resolved":
+                correctDate = parser.parse(alert['endsAt']).strftime('%Y-%m-%d %H:%M:%S')
+                message += "Resolved: "+correctDate
+            elif alert['status'] == "firing":
+                correctDate = parser.parse(alert['startsAt']).strftime('%Y-%m-%d %H:%M:%S')
+                message += "Started: "+correctDate
 
             bot.sendMessage(chat_id=chatID, text=message)
             return "Alert OK", 200
     except:
-        bot.sendMessage(chat_id=chatID, text="Error!")
-        return "Alert nOK", 200
+        bot.sendMessage(chat_id=chatID, text="Error to read content ---->"+str(request.get_data()))
+        return "Alert fail", 200
 
 
 if __name__ == '__main__':
-    logging.basicConfig(filename='./flaskAlert.log', level=logging.INFO)
-    app.run(host='0.0.0.0', port=9119, debug=True)
+    app.run(host='0.0.0.0', port=9119)
